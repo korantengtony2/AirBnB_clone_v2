f8f138f1527610f8df94800071e21f7a9e396f7f
Jasemonu <jasemonu@gmail.com>
2023-06-21 16:41:59 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,68 +1,43 @@
 #!/usr/bin/python3
-""" holds class User"""
-import hashlib
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.user import User
+import os
 
 
-class User(BaseModel, Base):
-<<<<<<< HEAD
-    """Represents a user for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table users.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
-=======
-    """Representation of a user """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
+class test_User(test_basemodel):
+    """ test class for user model"""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """ user test class init"""
         super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
 
-    @property
-    def password(self):
-        return self._password
+    def test_first_name(self):
+        """ testing user first anme attr"""
+        new = self.value()
+        self.assertEqual(type(new.first_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
->>>>>>> 736a57b6fd91cf572e49bb91456a97f302b36fc5
+    def test_last_name(self):
+        """ testing user last name attr"""
+        new = self.value()
+        self.assertEqual(type(new.last_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_email(self):
+        """ testing user email attr"""
+        new = self.value()
+        self.assertEqual(type(new.email), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_password(self):
+        """ testing user password attr"""
+        new = self.value()
+        self.assertEqual(type(new.password), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
